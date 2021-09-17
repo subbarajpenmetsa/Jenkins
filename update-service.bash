da33cf5f74a8f5d79bbe64062499382817baf3c6
subbarajpenmetsa <subbarajpenmetsa@gmail.com>
2021-09-17 19:20:12 +0530

    Update update-service.bash

--- update-service.bash
+++ update-service.bash
@@ -7,5 +7,5 @@ OLD_TASK_DEF=$(aws ecs describe-task-definition --region us-east-1 --task-defini
 NEW_TASK_DEF=$(echo $OLD_TASK_DEF | jq --arg NDI $NEW_DOCKER_IMAGE '.taskDefinition.containerDefinitions[0].image=$NDI')
 FINAL_TASK=$(echo $NEW_TASK_DEF | jq '.taskDefinition|{family: .family, volumes: .volumes, memory: .memory, containerDefinitions: .containerDefinitions}')
 echo $FINAL_TASK
-aws ecs register-task-definition --family $TASK_FAMILY --region us-east-1 --requires-compatibilities "FARGATE" --execution-role-arn "arn:aws:iam::779468985484:role/ecsTaskExecutionRole"  --cpu "256" --memory >
+ aws ecs register-task-definition --family $TASK_FAMILY --requires-compatibilities "FARGATE" --execution-role-arn "arn:aws:iam::779468985484:role/ecsTaskExecutionRole"  --cpu "256" --memory "512" --network-mode "awsvpc" --cli-input-json "$(echo $FINAL_TASK)"
 aws ecs update-service --service $SERVICE_NAME --region us-east-1 --task-definition $TASK_FAMILY --cluster $CLUSTER_NAME
